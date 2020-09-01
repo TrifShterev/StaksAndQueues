bf57cd882fffd473c85028ed3a3a27cd4ea48bbd
TrifShterev <tshterev@yahoo.com>
2020-09-01 10:50:14 +0300

    Add project files.

---
+++ Reverse String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
