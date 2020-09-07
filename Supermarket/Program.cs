f88c85a0026d12b860cd90236f94a9f74150abbf
TrifShterev <tshterev@yahoo.com>
2020-09-07 09:09:55 +0300

    Trafic Jam and Supermarket

---
+++ Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+
+            string input;
+
+            while ((input=Console.ReadLine())!="End")
+            {
+                if (input=="Paid")
+                {
+                    while (queue.Count>0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+                
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
---
+++ Traffic Jam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfCarsCanPass = int.Parse(Console.ReadLine());
+            var queueOfTheStreetLight = new Queue<string>();
+            int carsPassed = 0;
+            string input;
+            while ((input=Console.ReadLine())!= "end")
+            {
+                if (input=="green")
+                {
+
+                    for (int i = 0; i < numberOfCarsCanPass; i++)
+                    {
+                        if (queueOfTheStreetLight.Any())
+                        {
+                            Console.WriteLine($"{queueOfTheStreetLight.Dequeue()} passed!");
+                            carsPassed++;
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    queueOfTheStreetLight.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+        }
+    }
+}
