b98e88523afa0af69453f32fdeed21708e091908
TrifShterev <tshterev@yahoo.com>
2020-09-11 13:28:04 +0300

    added tasks

---
+++ Basic Queue Operations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int elementsToEnqueue = input[0];
+            int elementsDequeue = input[1];
+            int numberToFind = input[2];
+
+            
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> myQueue = new Queue<int>(numbers);
+            for (int i = 0; i < elementsDequeue; i++)
+            {
+                if (myQueue.TryDequeue(out int result))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine('0');
+                    return;
+                }
+            }
+            if (myQueue.Count > 0)
+            {
+
+
+                if (myQueue.Contains(numberToFind))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(myQueue.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine('0');
+            }
+        }
+    }
+}
---
+++ Basic Stack Operations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+            int elementsToPop = input[1];
+            int numberToFind = input[2];
+
+            
+           var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> playingStack = new Stack<int>(numbers);
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                if (playingStack.TryPop(out int result))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine('0');
+                    return;
+                }
+
+            }
+            if (playingStack.Contains(numberToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(playingStack.Min());
+            }
+        }
+    }
+}
